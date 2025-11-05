b5f2255aafd824428f522e614aa25dca21c8d41f
RaulGuadalajara <omargdl0506@gmail.com>
2025-11-05 01:09:55 -0600

    Update loginCotroller.js

--- app/controllers/loginCotroller.js
+++ app/controllers/loginCotroller.js
@@ -1,1 +1,13 @@
+angular.module('mathApp')
+.controller('LoginController', function($scope, $location, AuthService) {
+  $scope.user = { username: '', password: '' };
+  $scope.error = '';
 
+  $scope.login = function() {
+    if (AuthService.login($scope.user)) {
+      $location.path('/inicio');
+    } else {
+      $scope.error = 'Usuario o contraseña incorrectos.';
+    }
+  };
+});
