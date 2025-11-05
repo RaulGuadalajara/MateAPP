ffb6cbf8138cfaf1fb20d3e0b7123fbe1c66d97a
RaulGuadalajara <omargdl0506@gmail.com>
2025-11-05 01:10:31 -0600

    Update authServices.js

--- app/services/authServices.js
+++ app/services/authServices.js
@@ -1,1 +1,11 @@
 
+angular.module('mathApp')
+.factory('AuthService', function() {
+  const userData = { username: 'admin', password: '1234' };
+
+  return {
+    login: function(user) {
+      return user.username === userData.username && user.password === userData.password;
+    }
+  };
+});
